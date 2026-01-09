f0e751a9c49ea8cbd3bda4910a6dae240ca61b96
Amina Akhmetova <163754089+akhmetovamn@users.noreply.github.com>
2026-01-09 14:52:49 +0500

    Change page title from 'Oqu Easy - Register' to 'Register'

--- register.php
+++ register.php
@@ -86,7 +86,7 @@ $conn->close();
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Oqu Easy - Register</title>
+  <title>Register</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
